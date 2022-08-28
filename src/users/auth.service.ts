bc149e759a1cf5d0580b26d40120635bd24cc385
fahimSiam <siamfahim02@gmail.com>
2022-08-28 23:48:03 +0600

    c10e5 done

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 import { UsersService } from './users.service';
 
+const scrypt = promisify(_scrypt);
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
-  signup(email: string, password: string) {}
+  async signup(email: string, password: string) {
+    const users = await this.userService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email already used');
+    }
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
+  }
   signin() {}
 }
